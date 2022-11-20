633102d99330f19b114023ab0f906c6e58149c33
Kevin Martin Jose <jskevin@amazon.com>
2022-11-20 19:25:44 +0100

    Items api

--- pages/api/items.js
+++ pages/api/items.js
@@ -1,5 +1,7 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import data from './data.json' assert {type: 'json'};
 
 export default function handler(req, res) {
-  res.status(200).json({ name: 'John Doe' })
+
+  res.status(200).json(data);
 }
